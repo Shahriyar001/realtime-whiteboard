9eab602f9db23e32519805f805b606e640fe3f52
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-03-06 23:06:45 +0600

    added joinRoom logic

--- src/components/Forms/index.jsx
+++ src/components/Forms/index.jsx
@@ -16,7 +16,11 @@ const Forms = ({ uuid, socket, setUser }) => {
       </div>
       <div className="col-md-4 mt-5 form-box py-3 p-5 border  border-primary rounded-2 mx-auto d-flex flex-column align-items-center mx-auto">
         <h1 className="text-primary fw-bold">Join Room</h1>
-        <JoinRoomForm></JoinRoomForm>
+        <JoinRoomForm
+          uuid={uuid}
+          socket={socket}
+          setUser={setUser}
+        ></JoinRoomForm>
       </div>
     </div>
   );
--- src/components/Forms/joinRoomForm/index.jsx
+++ src/components/Forms/joinRoomForm/index.jsx
@@ -1,4 +1,27 @@
-const JoinRoomForm = () => {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const JoinRoomForm = ({ uuid, socket, setUser }) => {
+  const [roomId, setRoomId] = useState("");
+  const [name, setName] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleRoomJoin = (e) => {
+    e.preventDefault();
+
+    const roomData = {
+      name,
+      roomId,
+      userId: uuid(),
+      host: true,
+      presenter: true,
+    };
+    setUser(roomData);
+    navigate(`/${roomId}`);
+    socket.emit("userJoind", roomData);
+  };
+
   return (
     <form className="form col-md-12 mt-5">
       <div className="form-group">
@@ -6,6 +29,8 @@ const JoinRoomForm = () => {
           type="text"
           className="form-control my-2"
           placeholder="Enter Your Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
       </div>
       <div className="form-group ">
@@ -14,10 +39,13 @@ const JoinRoomForm = () => {
           type="text"
           className="form-control my-2 "
           placeholder="Enter rome code"
+          value={roomId}
+          onChange={(e) => setRoomId(e.target.value)}
         />
       </div>
       <button
         type="submit"
+        onClick={handleRoomJoin}
         className="mt-4 btn btn-primary btn-block form-control"
       >
         Join Room
